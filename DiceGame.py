d2c15db4ab12489aa1d2fe278c272666a849d6dc
am <am@hoxro.com>
2022-09-07 19:32:35 +0600

    s

--- DiceGame.py
+++ DiceGame.py
@@ -1,23 +1,20 @@
 import random
-
 def roll_dice():
     dice_total = random.randint(1, 3) + random.randint(1, 3)
     return dice_total
-
-player1 = input("Enter player1 Name: ")
-player2 = input("Enter player2 Name: ")
+p1 = input("Enter player1 Name: ")
+p2 = input("Enter player2 Name: ")
 
 roll1 = roll_dice()
 roll2 = roll_dice()
 
-
-print(player1, 'Rolled', roll1)
-print(player2, 'Rolled', roll2)
+print(p1, 'Rolled', roll1)
+print(p2, 'Rolled', roll2)
 
 if roll1 > roll2:
-    print(player1, "Win!")
+    print(p1, "Win!")
 elif roll2 > roll1:
-    print(player2, "Win!")
+    print(p2, "Win!")
 else:
     print("You Tie!")
 
